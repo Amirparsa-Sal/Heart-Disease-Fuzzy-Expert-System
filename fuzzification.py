37b7348c6ea546232ff17326ebbdf70ef06d11c0
Amirparsa-Sal <Amirparsa.s751@gmail.com>
2022-06-25 19:03:57 +0430

    add fuzzyset implementation

---
+++ fuzzification.py
@@ -0,0 +1,72 @@
+from abc import abstractmethod, ABC
+from typing import Tuple
+
+class FuzzySetSection(ABC):
+    
+    @abstractmethod
+    def range(self) -> Tuple:
+        pass
+    
+    @abstractmethod
+    def get_value(self, x: float) -> Tuple:
+        pass
+
+class Line(FuzzySetSection):
+    def __init__(self, start_pos: Tuple, end_pos: Tuple) -> None:
+        if start_pos[0] < end_pos[0]:
+            self.start_pos = start_pos
+            self.end_pos = end_pos
+        else:
+            self.start_pos = end_pos
+            self.end_pos = start_pos
+        self.m, self.b = self.find_line_paramaters(self.start_pos[0], self.start_pos[1], self.end_pos[0], self.end_pos[1])
+
+    def find_line_paramaters(self, x1: int, y1: int, x2: int, y2: int) -> Tuple:
+        m = (y2 - y1) / (x2 - x1)
+        b = y1 - m * x1
+        return m, b
+
+    def get_value(self, x: float) -> Tuple:
+        return self.m * x + self.b
+    
+    def range(self) -> Tuple:
+        return self.start_pos[0], self.end_pos[0]
+
+class ConstantValue:
+    def __init__(self, start_x: float, end_x: float, value: float) -> None:
+        self.start_x = start_x
+        self.end_x = end_x
+        self.value = value
+    
+    def get_value(self, x: float) -> float:
+        return self.value
+    
+    def range(self) -> Tuple:
+        return self.start_x, self.end_x
+
+class FuzzySet:
+
+    def __init__(self, name:str, range: Tuple) -> None:
+        self.name = name
+        self.range = range
+        self.sections = []
+
+    def add(self, section: FuzzySetSection) -> None:
+        for s in self.sections:
+            if self.__has_interception(s.range(), section.range()):
+                raise ValueError('Interception between sections')
+        self.sections.append(section)
+    
+    def get_value(self, x: float) -> float:
+        if x < self.range[0] or x > self.range[1]:
+            return ValueError('x out of range')
+
+        for section in self.sections:
+            if section.range()[0] <= x <= section.range()[1]:
+                return section.get_value(x)
+        return 0
+    
+    def __has_interception(self, range1: Tuple, range2: Tuple) -> bool:
+        if range2[0] < range1[1] < range2[1] or range1[0] < range2[1] < range1[1]:
+            return True
+        return False
