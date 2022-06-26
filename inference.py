b77af1bde038b787459a1a92827ae2b514b14128
Amirparsa-Sal <Amirparsa.s751@gmail.com>
2022-06-26 04:44:44 +0430

    implement calculation of inference engine

--- inference.py
+++ inference.py
@@ -1,4 +1,5 @@
-from fuzzification import init_fuzzy_parameters
+from ast import operator
+from fuzzification import init_fuzzy_parameters, init_output_fuzzy_sets
 from abc import ABC, abstractmethod
 from typing import Tuple, List
 from dataclasses import dataclass
@@ -41,14 +42,13 @@ class RuleTerm:
 class Rule:
     def __init__(self, if_clause: List, then_clause: Tuple) -> None:
         self.if_clause_items = []
+        self.then_clause_item = RuleTerm(then_clause[0], then_clause[1])
         self.operator = None
         for term in if_clause:
             if isinstance(term, Tuple):
                 self.if_clause_items.append(RuleTerm(term[0], term[1]))
             elif not self.operator:
                 self.operator = OperatorFactory.get_operator(term)
-            
-        self.then_clause_item = RuleTerm(then_clause[0], then_clause[1])
     
     def __str__(self) -> str:
         return '{} {} {}'.format(self.if_clause_items, self.operator, self.then_clause_item)
@@ -59,15 +59,43 @@ class FuzzyIntelligentSystem:
         self.fuzzy_rules = []
         self.__extract_rules(rules_file)
         self.fuzzy_parameters = dict()
+        self.output_param = init_output_fuzzy_sets()
         for param in init_fuzzy_parameters():
-            self.fuzzy_parameters[param.name] = param
+            self.fuzzy_parameters[param.name_in_rules] = param
 
     def __extract_rules(self, rules_file) -> None:
         with open(rules_file, 'r') as f:
             for line in f:
                 if_clause, then_clause = RuleParser.parse_rule(line[:-1])
                 self.fuzzy_rules.append(Rule(if_clause, then_clause))
-    
-fs = FuzzyIntelligentSystem()
-fs.extract_rules()
-print(fs.rules[0])
+
+    def calculate_result(self, input_dict: dict) -> float:
+        max_values = dict()
+        for key in self.output_param.sets.keys():
+            max_values[key] = 0
+        for rule in self.fuzzy_rules:
+            result_value = 0
+            term = rule.if_clause_items[0]
+            if term.parameter_name in input_dict:
+                if term.parameter_name in self.fuzzy_parameters:
+                    result_value = self.fuzzy_parameters[term.parameter_name].get_value_in_set(
+                    int(input_dict[term.parameter_name]), term.fuzzyset_name)
+                else:
+                    raise ValueError(f'Parameter {term.parameter_name} is not in fuzzy parameters')
+            else:
+                raise ValueError(f'Parameter {term.parameter_name} is not in input dict')
+            
+            for i, term in enumerate(rule.if_clause_items[1:]):
+                if term.parameter_name in input_dict:
+                    if term.parameter_name in self.fuzzy_parameters:
+                        term_value = self.fuzzy_parameters[term.parameter_name].get_value_in_set(
+                            int(input_dict[term.parameter_name]), term.fuzzyset_name)
+                        result_value = rule.operator.get_fuzzy_value(result_value, term_value)
+                    else:
+                        raise ValueError(f'Parameter {term.parameter_name} is not in fuzzy parameters')
+                else:
+                    raise ValueError(f'Parameter {term.parameter_name} is not in input dict')
+
+            if result_value > max_values[rule.then_clause_item.fuzzyset_name]:
+                max_values[rule.then_clause_item.fuzzyset_name] = result_value
+        return max_values
